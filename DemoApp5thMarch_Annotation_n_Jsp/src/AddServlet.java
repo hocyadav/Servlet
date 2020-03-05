01650d432cca0483312e66eb4b9dc1f260bf2a9b
Hariom Yadav <hariom.yadav@broadcom.com>
2020-03-05 11:25:55 +0530

    author added

--- DemoApp5thMarch_Annotation_n_Jsp/src/AddServlet.java
+++ DemoApp5thMarch_Annotation_n_Jsp/src/AddServlet.java
@@ -4,6 +4,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+/**
+ * 
+ * @author Hariom Yadav | 05-Mar-2020
+ *
+ */
 @WebServlet("/add")
 public class AddServlet extends HttpServlet{
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
